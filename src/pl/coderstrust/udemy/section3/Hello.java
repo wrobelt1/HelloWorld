7a7504b165a776b5ec2854c8b7759841fb153cf2
Tomek Wrobel <wrobelt1@gmail.com>
2018-12-19 20:40:43 +0100

    Kolejny commit

--- src/pl/coderstrust/udemy/section3/Hello.java
+++ src/pl/coderstrust/udemy/section3/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Druga linijka Hello World");
+        System.out.println("Trzecia linia");
     }
 }
